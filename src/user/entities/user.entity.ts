df55048da157ecbc0622bc508d409f0e96116475
Dixit Vora <dixitvora@gmail.com>
2025-11-19 00:00:21 +0530

    feat: added the entity file for the user

---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,46 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  Index,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity({ name: 'users' })
+export class UserEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'varchar', length: 100 })
+  firstName: string;
+
+  @Column({ type: 'varchar', length: 100 })
+  lastName: string;
+
+  @Index()
+  @Column({ type: 'varchar', length: 255, unique: true })
+  email: string;
+
+  @Column({ type: 'varchar', length: 255 })
+  password: string;
+
+  @Index()
+  @Column({ type: 'uuid' })
+  tenantId: string;
+
+  @Column({ type: 'boolean', default: true })
+  is_active: string;
+
+  @Column({ type: 'varchar', length: 255, nullable: true })
+  refresh_token: string;
+
+  @Column({ type: 'timestamp', nullable: true })
+  last_login_at: Date | null;
+
+  @CreateDateColumn({ type: 'timestamp with time zone' })
+  created_at: Date;
+
+  @UpdateDateColumn({ type: 'timestamp with time zone' })
+  updated_at: Date;
+}
